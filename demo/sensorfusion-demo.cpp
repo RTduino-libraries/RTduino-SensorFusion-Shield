db5b491beb36c4240f611ee2df9898c5d8c2f142
Yuqiang Wang <yifang.wangyq@foxmail.com>
2024-08-21 09:52:44 +0800

    RTduino-SensorFusion-Shield adapt (#1)
    * RTduino-SensorFusion-Shield adapt
    
    * update commit
    
    * merge drivers

--- demo/sensorfusion-demo.cpp
+++ demo/sensorfusion-demo.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2022, RT-Thread Development Team
+ * Copyright (c) 2006-2024, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -14,10 +14,10 @@ static void _senfu_setup(void)
     /* put your setup code here, to run once: */
 }
 
-static void _senfu_setup(void)
+static void _senfu_loop(void)
 {
     /* put your main code here, to run repeatedly: */
 
     delay(50);
 }
-RTDUINO_SKETCH_LOADER("senfu", _senfu_setup, _senfu_setup);
+RTDUINO_SKETCH_LOADER("senfu", _senfu_setup, _senfu_loop);
